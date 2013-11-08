468a7b60e49d3ce72f2add2d1794291c703fa395
Kevin Dawson <bowtie@cpan.org>
2013-11-08 22:03:47 +0000

    Import of BOWTIE/Padre-Plugin-Autodia-0.04 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-Autodia
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BOWTIE/Padre-Plugin-Autodia-0.04.tar.gz
    gitpan-cpan-author:       BOWTIE
    gitpan-cpan-maturity:     released

--- lib/Padre/Plugin/Autodia.pm
+++ lib/Padre/Plugin/Autodia.pm
@@ -1,11 +1,14 @@
 package Padre::Plugin::Autodia;
 
 # ABSTRACT: Autodia UML creator plugin for Padre
-use v5.10;
+use 5.010001;
 
 use strict;
 use warnings;
-our $VERSION = '0.03';
+# turn of experimental warnings
+no if $] > 5.017010, warnings => 'experimental::smartmatch';
+
+our $VERSION = '0.04';
 
 use Padre::Wx       ();
 use Padre::Constant ();
@@ -24,10 +27,7 @@ use parent qw(
 	Padre::Role::Task
 );
 
-use Data::Printer {
-	caller_info => 1,
-	colored     => 1,
-};
+use Data::Printer {caller_info => 1, colored => 1,};
 
 #######
 # Called by padre to know the plugin name
@@ -45,7 +45,7 @@ sub plugin_enable {
 	my $autodia_exists = 0;
 
 	try {
-		if ( File::Which::which('dia') ) {
+		if (File::Which::which('dia')) {
 			$autodia_exists = 1;
 		}
 	};
@@ -82,8 +82,9 @@ use constant CHILDREN => qw{
 sub menu_plugins_simple {
 	my $self = shift;
 	return $self->plugin_name => [
+
 		# Wx::gettext('About') => sub {
-			# $self->show_about;
+		# $self->show_about;
 		# },
 		Wx::gettext('UML jpg') => [
 			Wx::gettext('Class Diagram (Current File jpg)') => sub {
@@ -100,7 +101,7 @@ sub menu_plugins_simple {
 			Wx::gettext('Project Class Diagram (dia)') => sub {
 				$self->project_dia;
 			},
-		],		
+		],
 		Wx::gettext('About...') => sub {
 			$self->plugin_about;
 		},
@@ -110,24 +111,15 @@ sub menu_plugins_simple {
 my @files_found = ();
 
 # http://docs.wxwidgets.org/stable/wx_wxfiledialog.html
-my $orig_wildcards = join(
-	'|',
-	Wx::gettext("JavaScript Files"),
-	"*.js;*.JS",
-	Wx::gettext("Perl Files"),
-	"*.pm;*.PM;*.pl;*.PL",
-	Wx::gettext("PHP Files"),
-	"*.php;*.php5;*.PHP",
-	Wx::gettext("Python Files"),
-	"*.py;*.PY",
-	Wx::gettext("Ruby Files"),
-	"*.rb;*.RB",
-	Wx::gettext("SQL Files"),
-	"*.slq;*.SQL",
-	Wx::gettext("Text Files"),
-	"*.txt;*.TXT;*.yml;*.conf;*.ini;*.INI",
-	Wx::gettext("Web Files"),
-	"*.html;*.HTML;*.htm;*.HTM;*.css;*.CSS",
+my $orig_wildcards = join('|',
+	Wx::gettext("JavaScript Files"), "*.js;*.JS",
+	Wx::gettext("Perl Files"),       "*.pm;*.PM;*.pl;*.PL",
+	Wx::gettext("PHP Files"),        "*.php;*.php5;*.PHP",
+	Wx::gettext("Python Files"),     "*.py;*.PY",
+	Wx::gettext("Ruby Files"),       "*.rb;*.RB",
+	Wx::gettext("SQL Files"),        "*.slq;*.SQL",
+	Wx::gettext("Text Files"),       "*.txt;*.TXT;*.yml;*.conf;*.ini;*.INI",
+	Wx::gettext("Web Files"),        "*.html;*.HTML;*.htm;*.HTM;*.css;*.CSS",
 );
 
 # get language and wildcard
@@ -139,11 +131,16 @@ my $languages = {
 
 my $wildcards = join(
 	'|',
-	map { sprintf( Wx::gettext("%s Files"), $_ ) => join( ';', map ( "*$_", @{ $languages->{$_} } ) ) }
-		sort keys %$languages
+	map {
+		sprintf(Wx::gettext("%s Files"), $_) =>
+			join(';', map { "*$_", @{$languages->{$_}} })
+	} sort keys %$languages
 );
 
-$wildcards .= (Padre::Constant::WIN32) ? Wx::gettext("All Files") . "|*.*|" : Wx::gettext("All Files") . "|*|";
+$wildcards
+	.= (Padre::Constant::WIN32)
+	? Wx::gettext("All Files") . "|*.*|"
+	: Wx::gettext("All Files") . "|*|";
 
 
 sub draw_this_file {
@@ -155,10 +152,16 @@ sub draw_this_file {
 
 	my $outfile = "${filename}.draw_this_file.jpg";
 
-	( my $language = lc( $document->mimetype ) ) =~ s|application/[x\-]*||;
+	(my $language = lc($document->mimetype)) =~ s|application/[x\-]*||;
 
-	my $autodia_handler =
-		$self->_get_handler( { filenames => [$filename], outfile => $outfile, graphviz => 1, language => $language } );
+	my $autodia_handler = $self->_get_handler(
+		{
+			filenames => [$filename],
+			outfile   => $outfile,
+			graphviz  => 1,
+			language  => $language
+		}
+	);
 
 	my $processed_files = $autodia_handler->process();
 
@@ -176,10 +179,11 @@ sub draw_all_files {
 
 	# show dialog, get files
 	my $dialog = Wx::FileDialog->new(
-		Padre->ide->wx->main, Wx::gettext('Open File'),
+		Padre->ide->wx->main,
+		Wx::gettext('Open File'),
 		$directory, '', $wildcards, Wx::wxFD_MULTIPLE,
 	);
-	if ( $dialog->ShowModal == Wx::wxID_CANCEL ) {
+	if ($dialog->ShowModal == Wx::wxID_CANCEL) {
 		return;
 	}
 
@@ -189,17 +193,23 @@ sub draw_all_files {
 
 	# get language for first file
 	my $language = 'perl';
-	foreach my $this_language ( keys %$languages ) {
-		if ( grep { $filenames[0] =~ m/$_$/ } @{ $languages->{$this_language} } ) {
+	foreach my $this_language (keys %$languages) {
+		if (grep { $filenames[0] =~ m/$_$/ } @{$languages->{$this_language}}) {
 			$language = lc($this_language);
 			last;
 		}
 	}
 
 	# run autodia on files
-	my $outfile = Cwd::getcwd() . "/padre.draw_these_files.jpg";
-	my $autodia_handler =
-		$self->_get_handler( { filenames => \@filenames, outfile => $outfile, graphviz => 1, language => $language } );
+	my $outfile         = Cwd::getcwd() . "/padre.draw_these_files.jpg";
+	my $autodia_handler = $self->_get_handler(
+		{
+			filenames => \@filenames,
+			outfile   => $outfile,
+			graphviz  => 1,
+			language  => $language
+		}
+	);
 
 	my $processed_files = $autodia_handler->process();
 	$autodia_handler->output();
@@ -215,59 +225,26 @@ sub _get_handler {
 	my $args = shift;
 
 	my $config = {
-		language => $args->{language}, graphviz => $args->{graphviz} || 0,
-		use_stdout => 0, filenames => $args->{filenames}
+		language   => $args->{language},
+		graphviz   => $args->{graphviz} || 0,
+		use_stdout => 0,
+		filenames  => $args->{filenames}
 	};
 	$config->{templatefile} = $args->{template} || undef;
 	$config->{outputfile}   = $args->{outfile}  || "autodia-plugin.out";
-
+p $config;
 	# unless ($language_handlers) {
 	my $language_handlers = Autodia->getHandlers();
-
+p $language_handlers;
 	# }
-	my $handler_module = $language_handlers->{ lc( $args->{language} ) };
-	eval "require $handler_module" or die "can't run '$handler_module' : $@\n";
+	my $handler_module = $language_handlers->{lc($args->{language})};
+p $handler_module;
+	eval { require $handler_module }
+		or die "can't run '$handler_module' : $@\n";
 	my $handler = "$handler_module"->new($config);
-	p $handler;
+p $handler;
 	return $handler;
 }
-
-# sub old_show_about_old {
-	# my ($main) = @_;
-
-	# my $about = Wx::AboutDialogInfo->new;
-	# $about->SetName('Padre::Plugin::Autodia');
-	# $about->SetVersion($Padre::Plugin::Autodia::VERSION);
-	# $about->SetDescription( Wx::gettext('Integrating automated documentation into Padre IDE') );
-	# $about->SetCopyright( Wx::gettext('Copyright 2010') . ' Aaron Trevena' );
-
-	# # Only Unix/GTK native about box supports websites
-	# if (Padre::Constant::UNIX) {
-		# $about->SetWebSite('http://padre.perlide.org/');
-	# }
-
-	# $about->AddDeveloper('Aaron Trevena: teejay at cpan dot org');
-
-	# Wx::AboutBox($about);
-	# return;
-# }
-
-
-# sub show_about {
-    # my $self  = shift;
-    # my $info = Wx::AboutDialogInfo->new;
-
-    # $info->SetName('Padre::Plugin::Autodia');
-    # $info->SetVersion( $VERSION );
-    # $info->SetDescription( Wx::gettext('Generate UML Class documentation for Dia') );
-    # $info->SetCopyright( '(c) 2008-2012 The Padre development team' );
-    # $info->SetWebSite('http://padre.perlide.org/');
-    # $info->AddDeveloper( 'Aaron Trevena <teejay@cpan.org>' );
-    # $info->AddDeveloper( 'Ahmad M. Zawawi <ahmad.zawawi@gmail.com>' );
-    # $info->AddDeveloper( 'Kevin Dawson <bowtie@cpan.org>' );    
-
-    # Wx::AboutBox( $info );
-# }
 
 #######
 # Add icon to Plugin
@@ -275,10 +252,10 @@ sub _get_handler {
 sub plugin_icon {
 	my $class = shift;
 	my $share = $class->plugin_directory_share or return;
-	my $file  = File::Spec->catfile( $share, 'icons', '16x16', 'dia.png' );
+	my $file  = File::Spec->catfile($share, 'icons', '16x16', 'dia.png');
 	return unless -f $file;
 	return unless -r $file;
-	return Wx::Bitmap->new( $file, Wx::wxBITMAP_TYPE_PNG );
+	return Wx::Bitmap->new($file, Wx::wxBITMAP_TYPE_PNG);
 }
 
 ########
@@ -308,25 +285,27 @@ sub plugin_about {
 	my $self = shift;
 
 	my $share = $self->plugin_directory_share or return;
-	my $file = File::Spec->catfile( $share, 'icons', '48x48', 'dia.png' );
+	my $file = File::Spec->catfile($share, 'icons', '48x48', 'dia.png');
 	return unless -f $file;
 	return unless -r $file;
 
 	my $info = Wx::AboutDialogInfo->new;
 
-	$info->SetIcon( Wx::Icon->new( $file, Wx::wxBITMAP_TYPE_PNG ) );
+	$info->SetIcon(Wx::Icon->new($file, Wx::wxBITMAP_TYPE_PNG));
 	$info->SetName('Padre::Plugin::Autodia');
-	$info->SetVersion($VERSION);    
-	$info->SetDescription( Wx::gettext('Generate UML Class documentation for Dia') );
-    $info->SetCopyright( '(c) 2008-2012 The Padre development team' );
-    $info->SetWebSite('http://padre.perlide.org/');
+	$info->SetVersion($VERSION);
+	$info->SetDescription(
+		Wx::gettext('Generate UML Class documentation for Dia'));
+	$info->SetCopyright('(c) 2008-2012 The Padre development team');
+	$info->SetWebSite('http://padre.perlide.org/');
 	$info->AddDeveloper('Kevin Dawson <bowtie@cpan.org>');
-    $info->AddDeveloper( 'Ahmad M. Zawawi <ahmad.zawawi@gmail.com>' );
-    $info->AddDeveloper( 'Aaron Trevena <teejay@cpan.org>' );
+	$info->AddDeveloper('Ahmad M. Zawawi <ahmad.zawawi@gmail.com>');
+	$info->AddDeveloper('Aaron Trevena <teejay@cpan.org>');
+
 	# $info->SetArtists(
-		# [   'Scott Chacon <https://github.com/github/gitscm-next>',
-			# 'Licence <http://creativecommons.org/licenses/by/3.0/>'
-		# ]
+	# [   'Scott Chacon <https://github.com/github/gitscm-next>',
+	# 'Licence <http://creativecommons.org/licenses/by/3.0/>'
+	# ]
 	# );
 	Wx::AboutBox($info);
 	return;
@@ -343,6 +322,7 @@ sub class_dia {
 	my $document = $main->current->document;
 
 	# $document->filename
+	return;
 }
 
 sub project_jpg {
@@ -354,11 +334,12 @@ sub project_jpg {
 
 	# $document->project_dir =~ /\/(?<project>\w+)$/;
 
-	# my $outfile = File::Spec->catfile( $document->project_dir, "$+{project}.jpg" );
-	my @project_dir = File::Spec->splitdir( $document->project_dir );
+# my $outfile = File::Spec->catfile( $document->project_dir, "$+{project}.jpg" );
+	my @project_dir = File::Spec->splitdir($document->project_dir);
 
-	my $outfile = File::Spec->catfile( $document->project_dir, "$project_dir[-1].jpg" );
-	
+	my $outfile
+		= File::Spec->catfile($document->project_dir, "$project_dir[-1].jpg");
+
 	require Padre::Plugin::Autodia::Task::Autodia_cmd;
 
 	# # Fire the task
@@ -386,7 +367,8 @@ sub project_dia {
 	# $document->project_dir =~ /\/(?<project>\w+)$/;
 	# my @project_dir = File::Spec->splitdir( $document->project_dir );
 
-	my $outfile = File::Spec->catfile( $document->project_dir, "autodia.out.dia" );
+	my $outfile
+		= File::Spec->catfile($document->project_dir, "autodia.out.dia");
 
 	require Padre::Plugin::Autodia::Task::Autodia_dia;
 
@@ -426,14 +408,14 @@ sub on_finish {
 
 	$main->show_output(1);
 	$output->clear;
-	$output->AppendText( $task->{output} );
-	$output->AppendText( "Ouput written to -> $task->{outfile}" );
+	$output->AppendText($task->{output});
+	$output->AppendText("Ouput written to -> $task->{outfile}");
 
-	given ( $task->{outfile} ) {
+	given ($task->{outfile}) {
 		when (/.jpg$/) { Padre::Wx::launch_browser("file://$task->{outfile}") }
 		when (/.dia$/) { system "dia", $task->{outfile} }
 	}
-	
+
 	p $task;
 
 	return;
@@ -446,13 +428,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Padre::Plugin::Autodia - Draw UML Class diagrams from Padre, the Perl IDE,
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 
 =head1 DESCRIPTION
@@ -502,10 +486,11 @@ Declare the Padre interfaces this plugin uses
 
 =item * project_jpg
 
-=item * show_about
+=item * plugin_about
 
-show 'about' dialog
+=item * plugin_icon
 
+=item * plugin_name
 
 =back
 
@@ -515,7 +500,7 @@ L<Autodia>, L<GraphViz>, L<Padre>
 
 =head1 CREDITS
 
-Initial Development of version 0.01 sponsered by Connected-uk
+Initial Development of version 0.01 sponsored by Connected-uk
 
 
 =head1 AUTHORS
